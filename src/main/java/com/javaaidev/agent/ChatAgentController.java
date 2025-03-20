3ede01198801af6a8f604880159ae566d6cc1a32
Fu Cheng <alexcheng1982@gmail.com>
2025-03-21 06:14:41 +1300

    update chat agent ui

--- src/main/java/com/javaaidev/agent/ChatAgentController.java
+++ src/main/java/com/javaaidev/agent/ChatAgentController.java
@@ -1,6 +1,16 @@
 package com.javaaidev.agent;
 
+import com.javaaidev.chatagent.model.ChatRequest;
+import com.javaaidev.chatagent.model.ChatResponse;
+import com.javaaidev.chatagent.model.TextContentPart;
+import com.javaaidev.chatagent.model.ThreadAssistantMessage;
+import com.javaaidev.chatagent.model.ThreadUserMessage;
+import java.util.List;
+import java.util.stream.Stream;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/chat")
 public class ChatAgentController {
 
-  public record ChatRequest(String input) {
-
-  }
-
-  public record ChatResponse(String output) {
-
-  }
 
   private static final String SYSTEM_TEXT = "You are a chef who is proficient in various cuisines. Please answer users' questions about cooking.";
   private final ChatClient chatClient;
@@ -27,10 +30,28 @@ public class ChatAgentController {
 
   @PostMapping
   public ChatResponse chat(@RequestBody ChatRequest request) {
+    var messages = request.messages().stream().flatMap(message -> {
+      if (message instanceof ThreadUserMessage userMessage) {
+        return userMessage.content().stream().map(part -> {
+          if (part instanceof TextContentPart textContentPart) {
+            return new UserMessage(textContentPart.text());
+          }
+          return null;
+        });
+      } else if (message instanceof ThreadAssistantMessage assistantMessage) {
+        return assistantMessage.content().stream().map(part -> {
+          if (part instanceof TextContentPart textContentPart) {
+            return new AssistantMessage(textContentPart.text());
+          }
+          return null;
+        });
+      }
+      return Stream.of();
+    }).toList();
     var output = chatClient.prompt().system(SYSTEM_TEXT)
-        .user(request.input())
+        .messages(messages.toArray(new Message[0]))
         .call()
         .content();
-    return new ChatResponse(output);
+    return new ChatResponse(List.of(new TextContentPart(output)));
   }
 }
